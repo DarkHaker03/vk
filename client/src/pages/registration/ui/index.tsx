ea83bf0be478161e0e7ef95654445b9f49f0ba20
BlueAurum <yibbragimov880@gmail.com>
2022-10-20 10:10:04 +0300

    registration page

--- client/src/pages/registration/ui/index.tsx
+++ client/src/pages/registration/ui/index.tsx
@@ -1,10 +1,26 @@
-import { FC } from 'react';
-// Раскомменте как только соберётесь использовать стили
-// import styles from './styles.module.scss';
+import React from "react";
+import styles from "./styles.module.scss";
+import vkIcon from "../../../shared/icon/ui/vk.png";
+import Button from "shared/ui/button";
+import Input from "shared/ui/input";
 
-const Registration: FC = () => (
-  <div>
-    2
+const Registration = () => (
+  <div className={styles.registration}>
+    <div className={styles.registrationBlock}>
+      <img className={styles.vkIcon} src={vkIcon} alt="vk icon" />
+      <h1 className={styles.title}>Введите почту</h1>
+      <p className={styles.subTitle}>
+        Ваша почта будет использоваться для входа в аккаунт
+      </p>
+      <form className={styles.form}>
+        <Input className="formInput" type="email" placeholder="Введите ваш Email" />
+        <Button className="mainBtn">продолжить</Button>
+      </form>
+      <p className={styles.warningText}>
+        <span>Нажимая "Продолжить" вы принимаете</span> пользовательское
+        соглашение и политику конфедициальности
+      </p>
+    </div>
   </div>
 );
 
